67ebdfdac92e392182677f4e75efb95bdfd5dd87
torokv@vnet.hu <Kapitany>
2023-09-04 21:37:50 +0200

    Add project files.

---
+++ Szerviz.Lekerdezes/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Szerviz.Lekerdezes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
