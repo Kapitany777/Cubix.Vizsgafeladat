d17a4df393fa6e79dbfbf908a2491471bd4d2901
torokv@vnet.hu <Kapitany>
2023-09-06 22:52:31 +0200

    A webes modul létrehozása

---
+++ Bejelentes.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bejelentes.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
